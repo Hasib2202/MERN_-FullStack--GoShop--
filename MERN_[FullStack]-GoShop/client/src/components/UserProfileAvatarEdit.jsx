96f02b421d513d1c0778e1645e87f55e437409c8
MD MOSTOFA HASIB <90277109+Hasib2202@users.noreply.github.com>
2024-12-14 01:00:25 +0600

    Upload Image

--- MERN_[FullStack]-GoShop/client/src/components/UserProfileAvatarEdit.jsx
+++ MERN_[FullStack]-GoShop/client/src/components/UserProfileAvatarEdit.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { TbUserSquareRounded } from "react-icons/tb";
 import { useSelector } from "react-redux";
 
 const UserProfileAvatarEdit = () => {
   const user = useSelector((state) => state?.user);
+  const [loading,setLoading] = useState(false)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // handle avatar upload
+  };
   return (
     <section className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center p-4 bg-neutral-900 bg-opacity-60">
-      <div className="flex-col items-center justify-center w-full max-w-sm p-4 bg-white rounded">
+      <div className="flex flex-col items-center justify-center w-full max-w-sm p-4 bg-white rounded">
         <div className="flex items-center justify-center w-20 h-20 overflow-hidden bg-red-500 rounded-full drop-shadow-sm">
           {user.avatar ? (
             <img alt={user.name} src={user.avatar} className="w-full h-full" />
@@ -14,17 +19,19 @@ const UserProfileAvatarEdit = () => {
             <TbUserSquareRounded size={65} />
           )}
         </div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="uploadProfile">
-            <button className="px-4 py-1 my-3 text-sm border rounded border-primary-100 hover:bg-primary-200">
-              Upload
-            </button>
+            <div className="px-4 py-1 my-3 text-sm border rounded border-primary-100 hover:bg-primary-200">
+              {
+                loading ? "Loading..." : "Upload"
+              }
+            </div>
+            <input
+                type="file"
+                id="uploadProfile"
+                className="hidden"
+            />
           </label>
-          <input
-            type="file"
-            id="uploadProfile"
-            className="w-full p-2 my-3 border rounded border-primary-100"
-          />
         </form>
       </div>
     </section>
